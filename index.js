c7b8c5ec323e71e8a35f335f506701b94198770b
Edith <33231165+dithyapr@users.noreply.github.com>
2020-08-24 03:10:07 +0700

    switch id

--- index.js
+++ index.js
@@ -3,9 +3,16 @@ var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 var mysql = require('mysql');
 var robot = require('robotjs');
+var opn = require('opn');
 
 
-var id
+var playnum;
+
+var signnum;
+
+var test;
+
+var fullscreen = false;
 
 var con = mysql.createConnection({
   host: "localhost",
@@ -16,26 +23,131 @@ var con = mysql.createConnection({
 
 con.connect(function(err) {
   if (err) throw err;
+
+  con.query("SELECT * FROM signurl", function (err, result, fields) {
+      if (err) throw err;
+      // console.log(result);
+
+      // for( var i = 0; i< result.length; i++)
+      // {
+      //   var row = result[i];
+      //   console.log(row);
+      // }
+
+       var string=JSON.stringify(result);
+        // console.log(string);
+        signnum =  JSON.parse(string);
+       // to get one value here is the option
+        // console.log(signnum[1].url1);
+        });
 });
 
 
-con.query("SELECT * FROM signurl", function (err, result, fields) {
-    if (err) throw err;
-    console.log(result);
-  });
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
+  // robot.keyTap("f11");
+});
+
+app.get('/1', (req, res) => {
+  id = 0;
+  res.sendFile(__dirname + '/index.html');
+  
+  // robot.keyTap("f11");
+});
+
+app.get('/2', (req, res) => {
+  id = 1;
+  
+});
+
+app.get('/3', (req, res) => {
+  id = 2;
+  
+});
+
+app.get('/4', (req, res) => {
+  id = 3;
+  
+});
+
+app.get('/5', (req, res) => {
+  id = 4;
+  
+});
+
+app.get('/6', (req, res) => {
+  id = 5;
+  
+});
+
+app.get('/7', (req, res) => {
+  id = 6;
+  
+});
+
+app.get('/8', (req, res) => {
+  id = 7;
+  
+});
+
+app.get('/9', (req, res) => {
+  id = 8;
+  
+});
+
+app.get('/10', (req, res) => {
+  id = 9;
+  
 });
 
 app.get('/controller', (req, res) => {
   res.sendFile(__dirname + '/controller.html');
+  // robot.keyTap("f11");
 });
+
+var search = false;
 
 io.on('connection', (socket) => {
   socket.on('chat message', (msg) => {
     io.emit('chat message', msg);
     console.log(msg);
+    playnum = msg;
+    // search = true;
+
+    // if(search == true)
+    // {
+       switch(playnum)
+        {
+          case "1":
+          console.log("Play 1");
+
+            
+              opn(signnum[id].url);
+            
+          
+          break;
+
+          case "2":
+          console.log("Play 2");
+            opn(signnum[id].url1);
+
+          break;
+
+          case "3":
+          console.log("Play 3");
+            opn(signnum[id].url2);
+          break;
+
+          case "4":
+          console.log("Play 4");
+            opn(signnum[id].url3);
+          break;
+        }
+    // }
+
+    // search = false;
+   
   });
 });
 
@@ -43,19 +155,5 @@ http.listen(3000, () => {
   console.log('listening on *:3000');
 });
 
-function requestFullScreen(element) {
-    // Supports most browsers and their versions.
-    var requestMethod = element.requestFullScreen || element.webkitRequestFullScreen || element.mozRequestFullScreen || element.msRequestFullScreen;
 
-    if (requestMethod) { // Native full screen.
-        requestMethod.call(element);
-    } else if (typeof window.ActiveXObject !== "undefined") { // Older IE.
-        var wscript = new ActiveXObject("WScript.Shell");
-        if (wscript !== null) {
-            wscript.SendKeys("{F11}");
-        }
-    }
-}
 
-var elem = document.body; // Make the body go full screen.
-requestFullScreen(elem);
